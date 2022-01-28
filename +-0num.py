7ac8328e6692ef5da18b66890c3f73c6c1a54057
Sakshi Dherange <98327359+Radhakrishn15@users.noreply.github.com>
2022-01-28 11:33:40 -0800

    Add files via upload

---
+++ +-0num.py
@@ -0,0 +1,13 @@
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
+   
+"""output
+Enter a number: -8
+Negative number
+
+[Program finished]"""
